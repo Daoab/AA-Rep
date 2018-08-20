2cabc6d54b80f94764306493a30c6f587b98d670
Daoab <38359000+Daoab@users.noreply.github.com>
2018-08-21 00:58:35 +0200

    Added splash screen, music and fly circuit

---
+++ Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour {
+
+    [SerializeField] float loadTime = 7f;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.Play();
+        Invoke("LoadLevel", loadTime);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    void LoadLevel() {
+        /*int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);*/
+
+        SceneManager.LoadScene(1);
+    }
+
+}
