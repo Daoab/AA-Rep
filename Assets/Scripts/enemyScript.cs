f39a6fee3eabd6109f88da9247c76ced2f35ecbc
Daoab <38359000+Daoab@users.noreply.github.com>
2018-08-30 17:19:34 +0200

    Added enemy explosions and text UI

--- Assets/Scripts/enemyScript.cs
+++ Assets/Scripts/enemyScript.cs
@@ -3,18 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
+
 public class enemyScript : MonoBehaviour {
 
-    Collider collider;
+    [SerializeField] GameObject deathFx;
+    [SerializeField] Transform parent;
+
+    BoxCollider collider;
 
 	// Use this for initialization
 	void Start () {
+
         collider = gameObject.AddComponent<BoxCollider>();
         collider.isTrigger = false;
+        collider.size = new Vector3(0.3f, 0.3f, 0.3f);
+        collider.center = new Vector3(0f, 0f, 0f);
+
     }
 
     private void OnParticleCollision(GameObject other)
     {
+        GameObject fx = Instantiate(deathFx, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
