a1b2909133b3930bd1471ea75b7cdb76c5122dea
Daoab <38359000+Daoab@users.noreply.github.com>
2018-08-30 13:18:14 +0200

    Upadted enemy script

---
+++ Assets/Scripts/enemyScript.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour {
+
+    Collider collider;
+
+	// Use this for initialization
+	void Start () {
+        collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Destroy(gameObject);
+    }
+}
