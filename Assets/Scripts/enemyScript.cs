70a91766352451439efdd8941c2afccc27acd88b
Daoab <38359000+Daoab@users.noreply.github.com>
2018-08-31 15:30:57 +0200

    Score is now functional

--- Assets/Scripts/enemyScript.cs
+++ Assets/Scripts/enemyScript.cs
@@ -10,22 +10,39 @@ public class enemyScript : MonoBehaviour {
     [SerializeField] GameObject deathFx;
     [SerializeField] Transform parent;
 
+    [Header("Score Board")]
+
+    [SerializeField] int score = 10;
+    ScoreBoard scoreBoard;
+
     BoxCollider collider;
+    bool hit = true;// This prevents the score from increasing twice if 2 particles collide at the same time
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+         
+    }
 
+    private void AddBoxCollider()
+    {
         collider = gameObject.AddComponent<BoxCollider>();
         collider.isTrigger = false;
         collider.size = new Vector3(0.3f, 0.3f, 0.3f);
         collider.center = new Vector3(0f, 0f, 0f);
-
     }
 
     private void OnParticleCollision(GameObject other)
     {
-        GameObject fx = Instantiate(deathFx, transform.position, Quaternion.identity);
-        fx.transform.parent = parent;
-        Destroy(gameObject);
+        if (hit)
+        {
+            hit = false;
+            GameObject fx = Instantiate(deathFx, transform.position, Quaternion.identity);
+            fx.transform.parent = parent;
+            scoreBoard.ScoreHit(score);
+            Destroy(gameObject);
+        }
     }
 }
