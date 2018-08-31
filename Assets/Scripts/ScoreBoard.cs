c98809489a00535c48ed0191bf9b834e1a1bec8f
Daoab <38359000+Daoab@users.noreply.github.com>
2018-08-31 11:50:08 +0200

    Created ScoreBoard class

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+
+    [SerializeField] int scorePerHit = 10;
+    private int score;
+    Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+        scoreText = gameObject.GetComponent<Text>();
+        scoreText.text = score.ToString();
+	}
+
+    public void ScoreHit() {
+        score += scorePerHit;
+        scoreText.text = score.ToString();
+    }
+}
